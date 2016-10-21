f6bcb1c11f4113f12c711d66447a479167dc4dc1
silence940109 <695412269@qq.com>
2016-10-21 16:20:05 +0800

    redis操作

--- src/main/java/com/silence/redis/TestRedis.java
+++ src/main/java/com/silence/redis/TestRedis.java
@@ -1,6 +1,7 @@
 package com.silence.redis;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +17,7 @@ public class TestRedis {
 	@Before
 	public void setup() {
 		
-		jedis = new Jedis("localhost");
+		jedis = new Jedis("localhost",6379);
 
 	}
 	/**
@@ -59,7 +60,73 @@ public class TestRedis {
 		jedis.hdel("user","age");
 		System.out.println(jedis.hmget("user", "age")); //因为删除了，所以返回的是null  
 		System.out.println(jedis.hlen("user")); //返回key为user的键中存放的值的个数2 
+		System.out.println(jedis.exists("user"));//是否存在key为user的记录 返回true  
+		System.out.println(jedis.hkeys("user"));//返回map对象中的所有key 
+		System.out.println(jedis.hvals("user"));//返回map对象中的所有value
+		Iterator<String> iter=jedis.hkeys("user").iterator();
+		while (iter.hasNext()){  
+			String key = iter.next();  
+			System.out.println(key+":"+jedis.hmget("user",key));  
+		} 	
+	}
+	
+	/**
+	 * Liat操作
+	 */
+	@Test
+	public void testList(){
+		//开始前，先移除所有的内容
+		jedis.del("java framework");  
+		System.out.println(jedis.lrange("java framework",0,-1)); 
+		//先向key java framework中存放三条数据
+		jedis.lpush("java framework","spring");  
+		jedis.lpush("java framework", "struts");
+		jedis.lpush("java framework","hibernate");
+		//再取出所有数据jedis.lrange是按范围取出，
+		// 第一个是key，第二个是起始位置，第三个是结束位置，jedis.llen获取长度 -1表示取得所有
+		System.out.println(jedis.lrange("java framework",0,-1));
 		
+		jedis.rpush("java framework","spring");
+		jedis.rpush("java framework","struts");
+		jedis.rpush("java framework","hibernate"); 
+		System.out.println(jedis.lrange("java framework",0,-1));
+	}
+	
+	/**
+	 * Set操作
+	 */
+	@Test
+	public void testSet(){
+		//添加  
+		jedis.sadd("user","liuling");
+		jedis.sadd("user","xinxin");  
+		jedis.sadd("user","ling");
+		jedis.sadd("user","zhangxinxin");
+		jedis.sadd("user","who"); 
+		//移除noname  
+		jedis.srem("user","who"); 
+		System.out.println(jedis.smembers("user"));//获取所有加入的value 
+		System.out.println(jedis.sismember("user", "who"));//判断 who 是否是user集合的元素
+		System.out.println(jedis.srandmember("user"));  
+		System.out.println(jedis.scard("user"));//返回集合的元素个数  
+	}
+	
+	/**
+	 * 列表操作
+	 */
+	@Test
+	public void test(){
+		//jedis 排序  
+		//注意，此处的rpush和lpush是List的操作。是一个双向链表（但从表现来看的）
+		jedis.del("a");//先清除数据，再加入数据进行测试 
+		jedis.rpush("a", "1"); 
+		jedis.lpush("a","6"); 
+		jedis.lpush("a","3");
+		jedis.lpush("a","9");
+		System.out.println(jedis.lrange("a",0,-1));// [9, 3, 6, 1]  
+		System.out.println(jedis.sort("a")); //[1, 3, 6, 9]  //输入排序后结果  
+		System.out.println(jedis.lrange("a",0,-1));
 		
 	}
+
 }
